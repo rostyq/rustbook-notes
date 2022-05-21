b7ac21ca909b98506f12db3dd9f634c1d1addbb9
Rostyslav Bohomaz <rostyslav.db@gmail.com>
2022-05-21 23:54:31 +0300

    3.4. Comments

---
+++ 03.common-programming-concepts/4.comments/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    // hello, world
+    println!("Hello, world!");
+
+    // multiple
+    // lines
+
+    let _some = 4;  // annotation after
+
+    // annotation before
+    let _some = 5;
+}
