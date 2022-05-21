1d1f03a12a241564e09231e1bdddc5892ce88d21
Rostyslav Bohomaz <rostyslav.db@gmail.com>
2022-05-21 20:12:56 +0300

    2. Programming a Guessing Game

---
+++ 02.guessing-game/src/main.rs
@@ -0,0 +1,39 @@
+use rand::Rng;
+use std::{cmp::Ordering, io};
+
+fn main() {
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..101);
+    let mut attempts_count: u32 = 0;
+    println!("debug: secret_number {}", secret_number);
+
+    loop {
+        print!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read input.");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        attempts_count += 1;
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+
+    println!("Attempts: {}", attempts_count);
+}
